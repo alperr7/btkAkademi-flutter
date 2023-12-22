6db1e88ede2bc0f12c13e1d012653efcbeab2eac
ALPER KANDEMİR <104205810+alperr7@users.noreply.github.com>
2023-12-22 14:20:49 +0300

    l

--- btk_flutter/lib/main.dart
+++ btk_flutter/lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  String mesaj = "Merhaba ilk uygulamam!";
+
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Text("Merhaba ilk uygulamam"),
+        title: Text(mesaj),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Text("Gövde kısım"),
       ),
     ),
   ));
